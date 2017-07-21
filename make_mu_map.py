1a8256cf986347eb4e5d67ee2375499b0e7b40cb
Benjamin Nosarzewski <bln@rhel6-64a.slac.stanford.edu>
2017-07-21 14:40:45 -0700

    first commit

---
+++ make_mu_map.py
@@ -0,0 +1,59 @@
+from numpy import *
+
+N = 144
+W = 8.
+
+
+################################
+# new for HolsteinTc12b12
+
+folder = 1
+
+
+#1 outputfiles
+#2 outputfiles_lower_temps
+
+
+if folder == 1:
+    #outputfiles
+    #omegas = [0.5,1.0,2.0]
+    omegas = [0.4,1.2,2.0]
+    betas  = [0.8, 1.6, 2.4, 4.8, 8.0, 12.0]
+    lamb   = 0.3
+    num_mus = 11
+
+if folder == 2:
+    #outputfiles   lowertemps????????
+    #omegas = [0.5,1.0,2.0]
+    omegas = [0.4,1.2,2.0]
+    betas  = [4.8, 8.0, 12.0]
+    lamb   = 0.3
+    num_mus = 11
+
+
+
+
+
+save('omegas',omegas)
+save('betas',betas)
+save('N',N)
+
+###
+### Use the following for the moresweeps2_fix data (same for the not fix?)
+###
+
+
+mu_map = zeros([len(omegas), len(betas), num_mus])
+
+for i in range(len(omegas)):
+    omega = omegas[i]
+    for j in range(len(betas)):
+        
+        if folder==1 or folder==2:
+            mu_map[i,j,:] = linspace(-2.5-lamb*W,-lamb*W,num_mus)
+
+       
+                                           
+save('mu_map', mu_map)
+print 'done saving mu map'
+print shape(mu_map)
